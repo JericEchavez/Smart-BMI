d1bd7663b7975e158b29697f035deedb13efbcd4
Jeric Echavez <44309859+JericEchavez@users.noreply.github.com>
2020-01-04 16:33:12 +0800

    Added PrintUsingPython File

---
+++ Printer/PrintUsingPython.py
@@ -0,0 +1,3 @@
+import os
+#os.system("sudo chmod a+w /dev/usb/lp0")
+os.system("sudo echo -e '\n\n\nSmart BMI \n\n Height: 5.6 \n Weight: 50kg\n BMI: 20.45 \n Obesity Level: Normal \n\n\n\n' > /dev/usb/lp0")
